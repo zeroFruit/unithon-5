52eb2115a75a2739ef883d20d09446666a6e46f3
zerofruit <14wnrkim@gmail.com>
2017-07-29 16:31:47 +0900

    s3 업로드

--- server/controller/album.js
+++ server/controller/album.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import request from 'request';
+import formidable from 'formidable';
 import { uploadImg } from '../helpers/S3Service';
 import { MS_ROOT_URL, SUB_KEY } from '../config/MSCognitive';
 import logger from '../config/logger';
@@ -7,36 +9,36 @@ function addAlbum(req, res, next) {
   const form = new formidable.IncomingForm();
 
   form.parse(req, (err, fields, files) => {
-    const { name, path } = files['0'];
-
-    uploadImg(name, path, (err, data, link) => {
+    uploadImg(files, (err, result) => {
       if (err) {
-        logger.error('[middlewares/uploadImg]', err);
+        logger.error('[controller/addAlbum]', err);
         return res.status(500).json({ message: 'SERVER_ERROR' });
       }
-
-      req.body.link = link;
-      next();
+      return res.json({ message: 'SUCCESS' });
     });
   });
 }
 
 function analyzeImg(req, res) {
-  const url = _.pick(req.body, ['url']);
-  console.log('c', url);
-  axios({
-    method: 'post',
+  const url = _.pick(req.body, ['url']).url;
+  const options = {
     url: MS_ROOT_URL,
-    header: {
+    headers: {
       'Content-Type': 'application/json',
       'Ocp-Apim-Subscription-Key': SUB_KEY
     },
-    body: { url }
-  }).then(response => {
-    return res.json({ message: 'SUCCESS_MESSAGE' });
-  }).catch(err => {
-    logger.error('[controller/album/analyzeImg]', err);
-    return res.status(500).json({ message: 'SERVER_ERROR' });
+    body: {
+      url
+    },
+    method: 'post',
+    json: true
+  };
+  request(options, (err, response, body) => {
+    if (err) {
+      return res.status(500).json({ message: 'SERVER_ERROR' });
+    }
+    const { description: { tags, captions: { text, confidence } } } = body;
+    return res.json({ message: 'SUCCESS', tags, captions: { text, confidence } });
   });
 }
 
--- server/router/index.js
+++ server/router/index.js
@@ -2,4 +2,5 @@ import AlbumController from '../controller/album';
 
 module.exports = app => {
   app.post('/analysis', AlbumController.analyzeImg);
+  app.post('/album', AlbumController.addAlbum);
 };
